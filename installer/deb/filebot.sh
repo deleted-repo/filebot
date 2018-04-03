4927eafc720005c1e1c22922ab6b16f61acea02a
Reinhard Pointner <reinhard.pointner@gmail.com>
2018-04-03 14:05:42 +0800

    Test -Djdk.gtk.version=3

--- installer/deb/filebot.sh
+++ installer/deb/filebot.sh
@@ -9,4 +9,4 @@ fi
 # select application data folder
 APP_DATA="$HOME/.filebot"
 
-java @{java.application.options} -Dapplication.deployment=deb -DuseGVFS=true -Dnet.filebot.gio.GVFS="$XDG_RUNTIME_DIR/gvfs" -Dapplication.dir="$APP_DATA" -Djava.io.tmpdir="$APP_DATA/tmp" -Djna.library.path="$APP_ROOT" -Djava.library.path="$APP_ROOT" -Dnet.filebot.AcoustID.fpcalc="$APP_ROOT/fpcalc" $JAVA_OPTS -classpath "$APP_ROOT/*" @{main.class} "$@"
+java @{java.application.options} -Dapplication.deployment=deb -Djdk.gtk.version=3 -DuseGVFS=true -Dnet.filebot.gio.GVFS="$XDG_RUNTIME_DIR/gvfs" -Dapplication.dir="$APP_DATA" -Djava.io.tmpdir="$APP_DATA/tmp" -Djna.library.path="$APP_ROOT" -Djava.library.path="$APP_ROOT" -Dnet.filebot.AcoustID.fpcalc="$APP_ROOT/fpcalc" $JAVA_OPTS -classpath "$APP_ROOT/*" @{main.class} "$@"
