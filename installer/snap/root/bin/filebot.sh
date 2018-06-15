63099d98b63d07f70a6a96cb8f3fec53340dea2b
Reinhard Pointner <rednoah@filebot.net>
2018-06-16 01:48:15 +0700

    switch to classic confinment (still broken though)

--- installer/snap/root/bin/filebot.sh
+++ installer/snap/root/bin/filebot.sh
@@ -1,13 +1,8 @@
 #!/bin/sh
-export LANG="C.UTF-8"
-export LC_ALL="C.UTF-8"
-
-export JAVA_HOME="$SNAP/usr/lib/jre"
-export PATH="$JAVA_HOME/jre/bin:$PATH"
-export LD_LIBRARY_PATH="$SNAP/usr/lib/filebot/lib:$LD_LIBRARY_PATH"
+export APP_ROOT="$SNAP/filebot"
 
 export APP_DATA="$SNAP_USER_DATA/data"
 export APP_CACHE="$SNAP_USER_DATA/cache"
 export APP_PREFS="$SNAP_USER_DATA/prefs"
 
-"$JAVA_HOME/bin/java" @{java.application.options} -Dapplication.deployment=snap -Dapplication.update=skip -Dapplication.help=show -Djava.library.path="$LD_LIBRARY_PATH" -Djna.library.path="$LD_LIBRARY_PATH" -Djna.boot.library.path="$LD_LIBRARY_PATH" -Dfile.encoding="UTF-8" -Dsun.jnu.encoding="UTF-8" -Dawt.useSystemAAFontSettings=on -Dswing.aatext=true -Dnet.filebot.UserFiles.fileChooser=JavaFX -DuseGVFS=true -Dnet.filebot.gio.GVFS="/run/user/$(id -u)/gvfs" -Duser.home="$SNAP_USER_DATA" -Dapplication.dir="$APP_DATA" -Dapplication.cache="$APP_CACHE/ehcache.disk.store" -Djava.io.tmpdir="$APP_CACHE/java.io.tmpdir" -Djava.util.prefs.userRoot="$APP_PREFS/user" -Djava.util.prefs.systemRoot="$APP_PREFS/system" -Dnet.filebot.AcoustID.fpcalc="$SNAP/usr/bin/fpcalc" $JAVA_OPTS -classpath "$SNAP/usr/lib/filebot/jar/*" @{main.class} "$@"
+"$SNAP/jre/bin/java" -Djdk.gtk.version=2 @{java.application.options} -Dapplication.deployment=snap -Dapplication.update=skip -Dapplication.help=show -Djava.library.path="$APP_ROOT/lib" -Djna.library.path="$APP_ROOT/lib" -Djna.boot.library.path="$APP_ROOT/lib" -Dfile.encoding="UTF-8" -Dsun.jnu.encoding="UTF-8" -Dawt.useSystemAAFontSettings=on -Dswing.aatext=true -Dnet.filebot.UserFiles.fileChooser=JavaFX -DuseGVFS=true -Dnet.filebot.gio.GVFS="/run/user/$(id -u)/gvfs" -Duser.home="$SNAP_USER_DATA" -Dapplication.dir="$APP_DATA" -Dapplication.cache="$APP_CACHE/ehcache.disk.store" -Djava.io.tmpdir="$APP_CACHE/java.io.tmpdir" -Djava.util.prefs.userRoot="$APP_PREFS/user" -Djava.util.prefs.systemRoot="$APP_PREFS/system" -Dnet.filebot.AcoustID.fpcalc="$APP_ROOT/lib/fpcalc" $JAVA_OPTS -classpath "$APP_ROOT/jar/*" @{main.class} "$@"
