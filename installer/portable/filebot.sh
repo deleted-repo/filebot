9abbeec2487cfccd3df20e8c7b5e0b757e401b4b
Reinhard Pointner <reinhard.pointner@gmail.com>
2018-06-20 01:53:52 +0700

    -Dnet.filebot.media.ffprobe="ffprobe"

--- installer/portable/filebot.sh
+++ installer/portable/filebot.sh
@@ -33,8 +33,13 @@ EXTRACTOR="ApacheVFS"                   # use Apache Commons VFS2 with junrar pl
 # EXTRACTOR="SevenZipExecutable"        # use the 7z executable
 # EXTRACTOR="SevenZipNativeBindings"    # use the lib7-Zip-JBinding.so native library
 
+# choose media parser
+MEDIA_PARSER="libmediainfo"             # use libmediainfo
+# MEDIA_PARSER="ffprobe"                # use ffprobe
+
+
 # select application data folder
 APP_DATA="$APP_ROOT/data"
 
 # start filebot
-java @{java.application.options} -Dapplication.deployment=portable -Djava.awt.headless=true -Dfile.encoding="UTF-8" -Dsun.jnu.encoding="UTF-8" -Dnet.filebot.Archive.extractor="$EXTRACTOR" -Djna.boot.library.path="$PACKAGE_LIBRARY_PATH:$LD_LIBRARY_PATH" -Djna.library.path="$PACKAGE_LIBRARY_PATH:$LD_LIBRARY_PATH" -Djava.library.path="$PACKAGE_LIBRARY_PATH:$LD_LIBRARY_PATH" -Dnet.filebot.AcoustID.fpcalc="fpcalc" -Dapplication.dir="$APP_DATA" -Duser.home="$APP_DATA" -Djava.io.tmpdir="$APP_DATA/tmp" -Djava.util.prefs.PreferencesFactory=net.filebot.util.prefs.FilePreferencesFactory -Dnet.filebot.util.prefs.file="$APP_DATA/prefs.properties" $JAVA_OPTS -classpath "$APP_ROOT/jar/*" @{main.class} "$@"
+java @{java.application.options} -Dapplication.deployment=portable -Djava.awt.headless=true -Dfile.encoding="UTF-8" -Dsun.jnu.encoding="UTF-8" -Dnet.filebot.media.parser="$MEDIA_PARSER" -Dnet.filebot.media.ffprobe="ffprobe" -Dnet.filebot.Archive.extractor="$EXTRACTOR" -Djna.boot.library.path="$PACKAGE_LIBRARY_PATH:$LD_LIBRARY_PATH" -Djna.library.path="$PACKAGE_LIBRARY_PATH:$LD_LIBRARY_PATH" -Djava.library.path="$PACKAGE_LIBRARY_PATH:$LD_LIBRARY_PATH" -Dnet.filebot.AcoustID.fpcalc="fpcalc" -Dapplication.dir="$APP_DATA" -Duser.home="$APP_DATA" -Djava.io.tmpdir="$APP_DATA/tmp" -Djava.util.prefs.PreferencesFactory=net.filebot.util.prefs.FilePreferencesFactory -Dnet.filebot.util.prefs.file="$APP_DATA/prefs.properties" $JAVA_OPTS -classpath "$APP_ROOT/jar/*" @{main.class} "$@"
