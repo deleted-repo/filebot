16b3dc8ae278bb39216e52abed77e13c16def49b
Reinhard Pointner <reinhard.pointner@gmail.com>
2018-06-20 01:52:05 +0700

    -Dnet.filebot.media.ffprobe="/volume1/@appstore/MediaServer/bin/ffprobe"

--- installer/spk/package/filebot.sh
+++ installer/spk/package/filebot.sh
@@ -50,8 +50,13 @@ EXTRACTOR="ApacheVFS"                   # use Apache Commons VFS2 with junrar pl
 # EXTRACTOR="SevenZipExecutable"        # use the 7z executable
 # EXTRACTOR="SevenZipNativeBindings"    # use the lib7-Zip-JBinding.so native library
 
+# choose media parser
+MEDIA_PARSER="libmediainfo"             # use libmediainfo
+# MEDIA_PARSER="ffprobe"                # use ffprobe
+
+
 # select application data folder
 APP_DATA="$APP_ROOT/data/$USER"
 
 # start filebot
-java @{java.application.options} -Dapplication.deployment=spk -Djava.awt.headless=true -Dfile.encoding="UTF-8" -Dsun.jnu.encoding="UTF-8"  -Dnet.filebot.Archive.extractor="$EXTRACTOR" -Djna.library.path="$SYNO_LIBRARY_PATH:$PACKAGE_LIBRARY_PATH" -Djava.library.path="$SYNO_LIBRARY_PATH:$PACKAGE_LIBRARY_PATH" -Duser.home="$APP_DATA" -Dapplication.dir="$APP_DATA" -Djava.io.tmpdir="$APP_DATA/tmp" -Djava.util.prefs.PreferencesFactory=net.filebot.util.prefs.FilePreferencesFactory -Dnet.filebot.util.prefs.file="$APP_DATA/prefs.properties" $JAVA_OPTS -classpath "$APP_ROOT/*" @{main.class} "$@"
+java @{java.application.options} -Dapplication.deployment=spk -Djava.awt.headless=true -Dfile.encoding="UTF-8" -Dsun.jnu.encoding="UTF-8" -Dnet.filebot.media.parser="$MEDIA_PARSER" -Dnet.filebot.media.ffprobe="/volume1/@appstore/MediaServer/bin/ffprobe" -Dnet.filebot.Archive.extractor="$EXTRACTOR" -Djna.library.path="$SYNO_LIBRARY_PATH:$PACKAGE_LIBRARY_PATH" -Djava.library.path="$SYNO_LIBRARY_PATH:$PACKAGE_LIBRARY_PATH" -Duser.home="$APP_DATA" -Dapplication.dir="$APP_DATA" -Djava.io.tmpdir="$APP_DATA/tmp" -Djava.util.prefs.PreferencesFactory=net.filebot.util.prefs.FilePreferencesFactory -Dnet.filebot.util.prefs.file="$APP_DATA/prefs.properties" $JAVA_OPTS -classpath "$APP_ROOT/*" @{main.class} "$@"
