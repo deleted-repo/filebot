407a4a695d8e24f08d81ce35b542325b86d5585b
Reinhard Pointner <reinhard.pointner@gmail.com>
2018-06-23 20:34:55 +0700

    Make a separate PKG build so we can have APP bundles with and without embedded JRE (i.e. no embedded JRE for brew cask automatic install)

---
+++ installer/pkg/bin/filebot.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+FILEBOT_HOME="/Application/FileBot.app/Contents/Home"
+JAVA_HOME="$FILEBOT_HOME/PlugIns/jre-@{java.version}.jre/Contents/Home"
+
+# select application data folder
+APP_DATA="$HOME/.filebot"
+LIBRARY_PATH="$FILEBOT_HOME/MacOS"
+
+# start filebot
+"$JAVA_HOME/bin/java" -Dapplication.deployment=pkg -Dapple.awt.UIElement=true -Dnet.filebot.AcoustID.fpcalc="$LIBRARY_PATH/fpcalc" @{java.application.options} @{linux.application.options} $JAVA_OPTS $FILEBOT_OPTS -classpath "$FILEBOT_HOME/Java/*" @{main.class} "$@"
