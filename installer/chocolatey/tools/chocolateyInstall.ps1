4c9537d1e05582b1913fc594b058b0e651c6242a
Reinhard Pointner <reinhard.pointner@gmail.com>
2018-06-26 19:11:08 +0700

    # File Release System link.release.index: https://get.filebot.net/filebot/

--- installer/chocolatey/tools/chocolateyInstall.ps1
+++ installer/chocolatey/tools/chocolateyInstall.ps1
@@ -1,1 +1,1 @@
-Install-ChocolateyPackage -PackageName '@{package.name}' -FileType 'msi' -SilentArgs '/quiet' -Url64bit 'https://get.filebot.net/@{package.name}/@{release}/@{release}_x64.msi' -ChecksumType64 'sha256' -Checksum64 '@{x64.msi.sha256}'
+Install-ChocolateyPackage -PackageName '@{package.name}' -FileType 'msi' -SilentArgs '/quiet' -Url64bit '@{link.release.index}/@{release}/@{release}_x64.msi' -ChecksumType64 'sha256' -Checksum64 '@{x64.msi.sha256}'
