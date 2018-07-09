f6c908f427ed26607ccdcf5e7da45b3265bcc6d3
Reinhard Pointner <reinhard.pointner@gmail.com>
2018-07-10 02:24:09 +0700

    Recreate /usr/bin/filebot link each and every time the QNAP NAS boots up

--- installer/qpkg/shared/start-stop-status.sh
+++ installer/qpkg/shared/start-stop-status.sh
@@ -1,12 +1,23 @@
 #!/bin/sh
+CONF="/etc/config/qpkg.conf"
+QPKG_NAME="filebot"
+QPKG_ROOT=`/sbin/getcfg $QPKG_NAME Install_Path -f $CONF`
+
 
 case "$1" in
 	start)
-		exit 0
+		ENABLED=$(/sbin/getcfg $QPKG_NAME Enable -u -d FALSE -f $CONF)
+		if [ "$ENABLED" != "TRUE" ]; then
+			echo "$QPKG_NAME is disabled."
+			exit 1
+		fi
+
+		# create /usr/bin/[package] program link
+		ln -sf "$QPKG_ROOT/filebot.sh" "/usr/bin/filebot"
 	;;
 
 	stop)
-		exit 0
+		rm "/usr/bin/filebot"
 	;;
 
 	restart)
@@ -18,5 +29,6 @@ case "$1" in
 		echo "Usage: $0 {start|stop|restart}"
 		exit 1
 esac
+
 
 exit 0
