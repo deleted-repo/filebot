822fcfe81acdf859e2b1dea85bac02934a13c188
Reinhard Pointner <reinhard.pointner@gmail.com>
2018-07-17 20:45:41 +0700

    $SNAP_DATA is not writable (which makes it pretty stupid and useless, snap development as usual)

--- installer/snap/base/bin/filebot.sh
+++ installer/snap/base/bin/filebot.sh
@@ -5,7 +5,7 @@ export LC_ALL="en_US.UTF-8"
 export JAVA_HOME="$SNAP/jre"
 export FILEBOT_HOME="$SNAP/filebot"
 
-APP_DATA="$SNAP_USER_DATA"
+APP_DATA="$SNAP_USER_DATA/data"
 LIBRARY_PATH="$FILEBOT_HOME/lib"
 
-"$JAVA_HOME/bin/java" -Dapplication.deployment=snap -Dapplication.update=skip -Dnet.filebot.license="$SNAP_DATA/.license" -Dnet.filebot.AcoustID.fpcalc="$SNAP/usr/bin/fpcalc" @{java.application.options} @{linux.application.options} @{linux.desktop.application.options} @{linux.portable.application.options} $JAVA_OPTS $FILEBOT_OPTS -jar "$FILEBOT_HOME/jar/filebot.jar" "$@"
+"$JAVA_HOME/bin/java" -Dapplication.deployment=snap -Dapplication.update=skip -Dnet.filebot.AcoustID.fpcalc="$SNAP/usr/bin/fpcalc" @{java.application.options} @{linux.application.options} @{linux.desktop.application.options} @{linux.portable.application.options} $JAVA_OPTS $FILEBOT_OPTS -jar "$FILEBOT_HOME/jar/filebot.jar" "$@"
