ae9c7bc00bca3c19abf36b56bd742705b2d54976
Reinhard Pointner <reinhard.pointner@gmail.com>
2018-11-20 17:10:38 +0700

    Fix filebot.sh script for OpenJDK 11

--- installer/app/filebot.sh
+++ installer/app/filebot.sh
@@ -15,10 +15,11 @@ done
 # get canonical path
 BIN=`dirname "$PRG"`
 FILEBOT_HOME=`cd "$BIN/.." && pwd`
+JAVA_HOME="$FILEBOT_HOME/PlugIns/jdk-@{jre.version}.jdk/Contents/Home"
 
 # select application data folder
 APP_DATA="$HOME/.filebot"
 LIBRARY_PATH="$FILEBOT_HOME/MacOS"
 
 # start filebot
-/usr/libexec/java_home --failfast --version "@{jvm.version}+" --exec java -Dapplication.deployment=app -Dapple.awt.UIElement=true -Dnet.filebot.AcoustID.fpcalc="$LIBRARY_PATH/fpcalc" @{java.application.options} @{linux.application.options} $JAVA_OPTS $FILEBOT_OPTS -jar "$FILEBOT_HOME/Java/filebot.jar" "$@"
+"$JAVA_HOME/bin/java" -Dapplication.deployment=app -Dapple.awt.UIElement=true -Dnet.filebot.AcoustID.fpcalc="$LIBRARY_PATH/fpcalc" @{java.application.options} @{linux.application.options} $JAVA_OPTS $FILEBOT_OPTS --module-path "$JAVA_HOME/ext/modules/lib" --add-modules ALL-MODULE-PATH -jar "$FILEBOT_HOME/Java/filebot.jar" "$@"
