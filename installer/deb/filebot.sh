f82e684d7bab2dc20d7c252b7789fe6342fe4ad0
Reinhard Pointner <reinhard.pointner@gmail.com>
2018-11-22 17:43:41 +0700

    Build architecture independent DEB package (and relegate most native dependencies to 3rd party packages assumed to be installed via apt)

--- installer/deb/filebot.sh
+++ installer/deb/filebot.sh
@@ -8,6 +8,6 @@ fi
 
 # select application data folder
 APP_DATA="$HOME/.filebot"
-LIBRARY_PATH="$FILEBOT_HOME/lib"
+LIBRARY_PATH="$FILEBOT_HOME/lib/$(uname -m)"
 
 java -Dapplication.deployment=deb -Dnet.filebot.Archive.extractor=SevenZipExecutable @{java.application.options} @{linux.application.options} @{linux.desktop.application.options} $JAVA_OPTS $FILEBOT_OPTS -jar "$FILEBOT_HOME/jar/filebot.jar" "$@"
