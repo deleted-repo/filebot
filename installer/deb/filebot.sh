76cd14b11c0ce6f350b0b9ad8b3203335ca789f6
Reinhard Pointner <reinhard.pointner@gmail.com>
2018-11-25 02:34:19 +0700

    Fix DEB package

--- installer/deb/filebot.sh
+++ installer/deb/filebot.sh
@@ -8,6 +8,7 @@ fi
 
 # select application data folder
 APP_DATA="$HOME/.filebot"
-LIBRARY_PATH="$FILEBOT_HOME/lib/$(uname -m)"
+LIBRARY_PATH="$FILEBOT_HOME/lib/$(uname -m):/usr/lib/$(uname -m)-linux-gnu/jni"
+MODULE_PATH="/usr/share/openjfx/lib"
 
-java -Dapplication.deployment=deb -Dnet.filebot.Archive.extractor=SevenZipExecutable @{java.application.options} @{linux.application.options} @{linux.desktop.application.options} $JAVA_OPTS $FILEBOT_OPTS -jar "$FILEBOT_HOME/jar/filebot.jar" "$@"
+java -Dapplication.deployment=deb --module-path "$MODULE_PATH" --add-modules ALL-MODULE-PATH -Dnet.filebot.Archive.extractor=SevenZipExecutable @{java.application.options} @{linux.application.options} @{linux.desktop.application.options} $JAVA_OPTS $FILEBOT_OPTS -jar "$FILEBOT_HOME/jar/filebot.jar" "$@"
