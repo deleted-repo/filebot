e82370b57fbb9552c67bea30d1660489c60f0d6f
Reinhard Pointner <reinhard.pointner@gmail.com>
2018-11-26 04:05:12 +0700

    Add dedicated launcher for AUR

---
+++ installer/aur/filebot.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+FILEBOT_HOME="/usr/share/filebot"
+
+if [ -z "$HOME" ]; then
+	echo '$HOME must be set'
+	exit 1
+fi
+
+# select application data folder
+APP_DATA="$HOME/.filebot"
+LIBRARY_PATH="$FILEBOT_HOME/lib/$(uname -m):/usr/lib/$(uname -m)-linux-gnu/jni"
+MODULE_PATH="/usr/share/openjfx/lib"
+
+java -Dapplication.deployment=aur --module-path "$MODULE_PATH" --add-modules ALL-MODULE-PATH -Dnet.filebot.Archive.extractor=SevenZipExecutable @{java.application.options} @{linux.application.options} @{linux.desktop.application.options} $JAVA_OPTS $FILEBOT_OPTS -jar "$FILEBOT_HOME/jar/filebot.jar" "$@"
