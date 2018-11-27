289ebbfcf840c064bfa4d0a2771a30abd70271bf
Reinhard Pointner <reinhard.pointner@gmail.com>
2018-11-27 17:06:41 +0700

    AUR: There currently is no JavaFX 11 package, so we'll just guess what the path might be, and modify later when there's an official package.

--- installer/aur/filebot.sh
+++ installer/aur/filebot.sh
@@ -8,7 +8,7 @@ fi
 
 # select application data folder
 APP_DATA="$HOME/.filebot"
-LIBRARY_PATH="$FILEBOT_HOME/lib/$(uname -m):/usr/lib/$(uname -m)-linux-gnu/jni"
-MODULE_PATH="/usr/share/openjfx/lib"
+LIBRARY_PATH="$FILEBOT_HOME/lib/$(uname -m)"
+MODULE_PATH="/usr/lib/openjfx/lib"
 
 java -Dapplication.deployment=aur --module-path "$MODULE_PATH" --add-modules ALL-MODULE-PATH -Dnet.filebot.Archive.extractor=SevenZipExecutable @{java.application.options} @{linux.application.options} @{linux.desktop.application.options} $JAVA_OPTS $FILEBOT_OPTS -jar "$FILEBOT_HOME/jar/filebot.jar" "$@"
