f3cbde26278481f1df0bf7f3485cc507cd5a4e28
Reinhard Pointner <reinhard.pointner@gmail.com>
2018-11-29 04:56:53 +0700

    + Experimental platform-independent DEB package (that relies on existing debian dependencies for all native code)

--- installer/deb-universal/filebot.sh
+++ installer/deb-universal/filebot.sh
@@ -9,5 +9,6 @@ fi
 # select application data folder
 APP_DATA="$HOME/.filebot"
 LIBRARY_PATH="/usr/lib/$(uname -m)-linux-gnu/jni"
+MODULE_PATH="/usr/share/openjfx/lib"
 
-java -Dapplication.deployment=deb -Djna.boot.library.name=jnidispatch.system -Dnet.filebot.Archive.extractor=SevenZipExecutable @{java.application.options} @{linux.application.options} @{linux.desktop.application.options} $JAVA_OPTS $FILEBOT_OPTS -jar "$FILEBOT_HOME/jar/filebot.jar" "$@"
+java -Dapplication.deployment=deb --module-path "$MODULE_PATH" --add-modules ALL-MODULE-PATH -Djna.boot.library.name=jnidispatch.system -Dnet.filebot.Archive.extractor=SevenZipExecutable @{java.application.options} @{linux.application.options} @{linux.desktop.application.options} $JAVA_OPTS $FILEBOT_OPTS -jar "$FILEBOT_HOME/jar/filebot.jar" "$@"
