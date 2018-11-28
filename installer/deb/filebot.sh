a3c0323fd60795c90fbbf0ca6fbb9a3698a818f3
Reinhard Pointner <reinhard.pointner@gmail.com>
2018-11-28 23:15:37 +0700

    Revert to amd64 only DEB package due to JavaFX native dependencies
    ➔ https://www.filebot.net/forums/viewtopic.php?f=11&t=6028&p=40242#p40241

--- installer/deb/filebot.sh
+++ installer/deb/filebot.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 FILEBOT_HOME="/usr/share/filebot"
+JAVA_HOME="/usr/lib/jvm/java-11-openjdk-amd64"
 
 if [ -z "$HOME" ]; then
 	echo '$HOME must be set'
@@ -8,7 +9,7 @@ fi
 
 # select application data folder
 APP_DATA="$HOME/.filebot"
-LIBRARY_PATH="$FILEBOT_HOME/lib/$(uname -m):/usr/lib/$(uname -m)-linux-gnu/jni"
-MODULE_PATH="/usr/share/openjfx/lib"
+LIBRARY_PATH="$FILEBOT_HOME/lib"
+MODULE_PATH="$FILEBOT_HOME/mod"
 
-java -Dapplication.deployment=deb --module-path "$MODULE_PATH" --add-modules ALL-MODULE-PATH -Dnet.filebot.Archive.extractor=SevenZipExecutable @{java.application.options} @{linux.application.options} @{linux.desktop.application.options} $JAVA_OPTS $FILEBOT_OPTS -jar "$FILEBOT_HOME/jar/filebot.jar" "$@"
+$JAVA_HOME/bin/java -Dapplication.deployment=deb --module-path "$MODULE_PATH" --add-modules ALL-MODULE-PATH -Dnet.filebot.Archive.extractor=SevenZipExecutable @{java.application.options} @{linux.application.options} @{linux.desktop.application.options} $JAVA_OPTS $FILEBOT_OPTS -jar "$FILEBOT_HOME/jar/filebot.jar" "$@"
