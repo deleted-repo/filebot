11054d2fefefc54d333e775cdffaf67a46ecdb3c
Reinhard Pointner <reinhard.pointner@gmail.com>
2018-11-29 13:02:35 +0700

    Make sure that Java 12 forwards compatibility is not an issue

--- installer/deb/filebot.sh
+++ installer/deb/filebot.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 FILEBOT_HOME="/usr/share/filebot"
-JAVA_HOME="/usr/lib/jvm/java-@{jvm.version}-openjdk-amd64"
 
 if [ -z "$HOME" ]; then
 	echo '$HOME must be set'
@@ -12,4 +11,4 @@ APP_DATA="$HOME/.filebot"
 LIBRARY_PATH="$FILEBOT_HOME/lib"
 MODULE_PATH="$FILEBOT_HOME/mod"
 
-$JAVA_HOME/bin/java -Dapplication.deployment=deb --module-path "$MODULE_PATH" --add-modules ALL-MODULE-PATH -Dnet.filebot.Archive.extractor=SevenZipExecutable @{java.application.options} @{linux.application.options} @{linux.desktop.application.options} $JAVA_OPTS $FILEBOT_OPTS -jar "$FILEBOT_HOME/jar/filebot.jar" "$@"
+java -Dapplication.deployment=deb --module-path "$MODULE_PATH" --add-modules ALL-MODULE-PATH -Dnet.filebot.Archive.extractor=SevenZipExecutable @{java.application.options} @{linux.application.options} @{linux.desktop.application.options} $JAVA_OPTS $FILEBOT_OPTS -jar "$FILEBOT_HOME/jar/filebot.jar" "$@"
