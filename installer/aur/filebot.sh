20dd96eb06383a81528ec33784c2e149195addd6
Reinhard Pointner <rednoah@filebot.net>
2018-12-18 16:27:34 +0700

    Fix Java 8 AUR build

--- installer/aur/filebot.sh
+++ installer/aur/filebot.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 FILEBOT_HOME="/usr/share/filebot"
+JAVA_HOME="/usr/lib/jvm/java-8-openjdk"
 
 if [ -z "$HOME" ]; then
 	echo '$HOME must be set'
@@ -7,8 +8,7 @@ if [ -z "$HOME" ]; then
 fi
 
 # select application data folder
-APP_DATA="$HOME/.filebot"
+APP_DATA="$HOME/.config/filebot"
 LIBRARY_PATH="$FILEBOT_HOME/lib/$(uname -m)"
-MODULE_PATH="/usr/lib/openjfx/lib"
 
-java -Dapplication.deployment=aur --module-path "$MODULE_PATH" --add-modules ALL-MODULE-PATH -Dnet.filebot.Archive.extractor=SevenZipExecutable @{java.application.options} @{linux.application.options} @{linux.desktop.application.options} $JAVA_OPTS $FILEBOT_OPTS -jar "$FILEBOT_HOME/jar/filebot.jar" "$@"
+$JAVA_HOME/bin/java -Dapplication.deployment=aur -Dnet.filebot.Archive.extractor=SevenZipExecutable @{java.application.options} @{linux.application.options} @{linux.desktop.application.options} $JAVA_OPTS $FILEBOT_OPTS -jar "$FILEBOT_HOME/jar/filebot.jar" "$@"
