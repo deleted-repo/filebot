d52a8e5d35b9737a1ae3b63a995e2191ed302f85
Reinhard Pointner <reinhard.pointner@gmail.com>
2019-03-05 23:20:56 +0700

    # select libjnidispatch.so from lib or lib64

--- installer/rpm/usr/share/filebot/bin/filebot.sh
+++ installer/rpm/usr/share/filebot/bin/filebot.sh
@@ -8,6 +8,8 @@ fi
 
 # select application data folder
 APP_DATA="$HOME/.filebot"
-LIBRARY_PATH="/usr/lib64/jna"
+
+# select libjnidispatch.so from lib or lib64
+LIBRARY_PATH=/usr/lib*/jna
 
 java -Dapplication.deployment=rpm -Djna.boot.library.name=jnidispatch -Dnet.filebot.archive.extractor=ShellExecutables @{java.application.options} @{linux.application.options} @{linux.desktop.application.options} $JAVA_OPTS $FILEBOT_OPTS -jar "$FILEBOT_HOME/jar/filebot.jar" "$@"
