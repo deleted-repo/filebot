d3905ab59b257e90541b69acf25bee6e0bce5d5a
Reinhard Pointner <reinhard.pointner@gmail.com>
2019-03-05 23:20:45 +0700

    # select libjnidispatch.system.so from $(uname -m)-linux-gnu folder

--- installer/deb-universal/filebot.sh
+++ installer/deb-universal/filebot.sh
@@ -8,7 +8,9 @@ fi
 
 # select application data folder
 APP_DATA="$HOME/.filebot"
-LIBRARY_PATH="/usr/lib/$(uname -m)-linux-gnu/jni"
-MODULE_PATH="/usr/share/openjfx/lib"
+
+# select libjnidispatch.system.so from $(uname -m)-linux-gnu folder
+LIBRARY_PATH=/usr/lib/*-linux-gnu*/jni
+MODULE_PATH=/usr/share/openjfx/lib
 
 java -Dapplication.deployment=deb --module-path "$MODULE_PATH" --add-modules ALL-MODULE-PATH -Djna.boot.library.name=jnidispatch.system -Dnet.filebot.archive.extractor=ShellExecutables @{java.application.options} @{linux.application.options} @{linux.desktop.application.options} $JAVA_OPTS $FILEBOT_OPTS -jar "$FILEBOT_HOME/jar/filebot.jar" "$@"
