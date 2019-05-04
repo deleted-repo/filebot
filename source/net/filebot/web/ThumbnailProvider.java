58ee860cc431ebd3d6b85259182993adab5e7626
Reinhard Pointner <reinhard.pointner@gmail.com>
2019-05-04 18:12:13 +0700

    Add caching

--- source/net/filebot/web/ThumbnailProvider.java
+++ source/net/filebot/web/ThumbnailProvider.java
@@ -7,6 +7,9 @@ import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
 import java.util.concurrent.CompletableFuture;
 
+import net.filebot.Cache;
+import net.filebot.CacheType;
+
 public enum ThumbnailProvider {
 
 	TheTVDB, TheMovieDB;
@@ -16,24 +19,38 @@ public enum ThumbnailProvider {
 	}
 
 	public byte[][] getThumbnails(int[] ids) throws Exception {
-		HttpClient client = HttpClient.newHttpClient();
-
 		CompletableFuture<HttpResponse<byte[]>>[] request = new CompletableFuture[ids.length];
 		byte[][] response = new byte[ids.length][];
 
+		// check cache
 		for (int i = 0; i < request.length; i++) {
-			HttpRequest r = HttpRequest.newBuilder(getThumbnailURL(ids[i])).build();
-			request[i] = client.sendAsync(r, BodyHandlers.ofByteArray());
+			response[i] = (byte[]) cache.get(ids[i]);
 		}
 
 		for (int i = 0; i < request.length; i++) {
-			HttpResponse<byte[]> r = request[i].get();
-			if (r.statusCode() == 200) {
-				response[i] = r.body();
+			if (response[i] == null) {
+				HttpRequest r = HttpRequest.newBuilder(getThumbnailURL(ids[i])).build();
+				request[i] = http.sendAsync(r, BodyHandlers.ofByteArray());
+			}
+		}
+
+		for (int i = 0; i < request.length; i++) {
+			if (response[i] == null) {
+				HttpResponse<byte[]> r = request[i].get();
+				if (r.statusCode() == 200) {
+					response[i] = r.body();
+					cache.put(ids[i], response[i]);
+				}
 			}
 		}
 
 		return response;
 	}
+
+	// per instance cache
+	private final Cache cache = Cache.getCache("thumbnail_" + ordinal(), CacheType.Persistent);
+
+	// shared instance for all thumbnail requests
+	private static final HttpClient http = HttpClient.newHttpClient();
 
 }
